6c97392a5ef07b478b57f8b713ca7c1e8de99611
Jis <jis@bairuhatech.com>
2025-06-30 11:24:12 +0530

    update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,7 +10,7 @@ import { User } from './user.entity';
 import { CreateUserDto } from './dto/create.dto';
 import { QueryUserDto } from './dto/query.dto';
 import { UpdateUserDto } from './dto/update.dto';
-import { DataResponseDto } from 'src/shared/dto/data-response.dto';
+import { DataResponseDto } from '../shared/dto/data-response.dto';
 
 @Injectable()
 export class UserService {
