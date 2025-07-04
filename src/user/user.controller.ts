962f47d4900625d2086900160801117f3e4525b0
Jis <jis@bairuhatech.com>
2025-07-04 10:09:56 +0530

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,10 +19,9 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create.dto';
 import { UpdateUserDto } from './dto/update.dto';
 import { QueryUserDto } from './dto/query.dto';
-import { AuthGuard } from '../shared/guards/auth.guard';
-
-import { DataResponseDto } from '../shared/dto/data-response.dto';
-import { Public } from '../shared/decorators/public.decorator';
+import { AuthGuard } from '@/shared/guards/auth.guard';
+import { DataResponseDto } from '@/shared/dto/data-response.dto';
+import { Public } from '@/shared/decorators/public.decorator';
 
 @ApiTags('User')
 @Controller('user')
